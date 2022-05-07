549ac7e5ed78fdebbba9e3a861768db8a1de0e5f
Linhx <phantranthelinh@gmail.com>
2022-05-07 20:09:19 +0700

    up

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.use("/api/orders", orderRouter)
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 1000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`server running on port ${PORT}`))
