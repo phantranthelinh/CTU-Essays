366facf37c48fb80b12961aa68490d22dcfba99a
pttlinh <pttlinh@gmail.com>
2022-12-09 15:00:48 +0700

    fix

--- src/Redux/Actions/CartActions.js
+++ src/Redux/Actions/CartActions.js
@@ -13,7 +13,7 @@ import {URL} from "../Url"
 //ADD TO CART
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/products/${id}`);
+  const { data } = await axios.get(`${URL}/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
